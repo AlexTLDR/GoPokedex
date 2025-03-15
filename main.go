657cb1985749fe85dc4b5c5be15cffa591bc5d96
AlexTLDR <alex.badragan@protonmail.com>
2025-03-15 19:49:55 +0100

    completed help and exit commands

--- main.go
+++ main.go
@@ -13,16 +13,26 @@ type cliCommand struct {
 	callback    func() error
 }
 
+var commands = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+}
+
+func init() {
+	commands["help"] = cliCommand{
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	}
+}
+
 func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
-	commands := map[string]cliCommand{
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-	}
+
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
@@ -63,5 +73,13 @@ func cleanInput(text string) []string {
 func commandExit() error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
 	return nil
 }

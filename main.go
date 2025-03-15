51060ca1f59e419403b9b618cc4dde8c5e6d6fa5
AlexTLDR <alex.badragan@protonmail.com>
2025-03-15 19:20:58 +0100

    exit command

--- main.go
+++ main.go
@@ -7,21 +7,44 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
-
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		input := scanner.Text()
 
 		cleaned := cleanInput(input)
-		if len(cleaned) > 0 {
-			firstWord := cleaned[0]
-			fmt.Printf("Your command was: %s\n", firstWord)
+		if len(cleaned) == 0 {
+			continue
 		}
+		command := cleaned[0]
+
+		if cmd, ok := commands[command]; ok {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("Invalid command")
+		}
+
 	}
+
 }
 
 func cleanInput(text string) []string {
@@ -35,4 +58,10 @@ func cleanInput(text string) []string {
 		cleaned = append(cleaned, strings.ToLower(part))
 	}
 	return cleaned
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
 }

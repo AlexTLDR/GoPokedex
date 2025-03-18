d494f13a60f61b25afdf6fd20c26484597c23acb
AlexTLDR <alex.badragan@protonmail.com>
2025-03-18 11:30:19 +0100

    refactored the commands logic

---
+++ internal/commands/commands.go
@@ -0,0 +1,186 @@
+package commands
+
+import (
+	"fmt"
+	"github.com/AlexTLDR/GoPokedex/internal/pokeapi"
+	"math/rand/v2"
+	"os"
+)
+
+type CommandFunc func(cfg *Config) error
+
+type Command struct {
+	Exec        CommandFunc
+	Description string
+}
+
+type Config struct {
+	Next          string
+	Previous      string
+	Args          []string
+	CaughtPokemon map[string]pokeapi.Pokemon
+}
+
+var Commands map[string]Command
+var client pokeapi.Client
+
+func Initialize() {
+	client = pokeapi.New()
+
+	Commands = map[string]Command{
+		"map": {
+			Exec:        CommandMap,
+			Description: "Display the names of 20 location areas in the Pokemon world",
+		},
+		"mapb": {
+			Exec:        CommandMapBack,
+			Description: "Go back to the previous list of location areas",
+		},
+		"help": {
+			Exec:        CommandHelp,
+			Description: "Displays a help message",
+		},
+		"exit": {
+			Exec:        CommandExit,
+			Description: "Exit the Pokedex",
+		},
+		"explore": {
+			Exec:        CommandExplore,
+			Description: "Explore the Pokemon world",
+		},
+		"catch": {
+			Exec:        CommandCatch,
+			Description: "Attempt to catch a Pokemon",
+		},
+	}
+}
+
+func ExecuteCommand(cmdName string, cfg *Config, args []string) error {
+	if cmd, exists := Commands[cmdName]; exists {
+		cfg.Args = args
+		return cmd.Exec(cfg)
+	}
+	return fmt.Errorf("unknown command: %s", cmdName)
+}
+
+func CommandExit(cfg *Config) error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func CommandHelp(cfg *Config) error {
+	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
+	for name, cmd := range Commands {
+		fmt.Printf("%s: %s\n", name, cmd.Description)
+	}
+	return nil
+}
+
+func CommandMap(cfg *Config) error {
+	locationResp, err := client.ListLocationAreas(cfg.Next)
+	if err != nil {
+		return err
+	}
+
+	if locationResp.Next != nil {
+		cfg.Next = *locationResp.Next
+	} else {
+		cfg.Next = ""
+	}
+
+	if locationResp.Previous != nil {
+		cfg.Previous = *locationResp.Previous
+	} else {
+		cfg.Previous = ""
+	}
+
+	for _, location := range locationResp.Results {
+		fmt.Println(location.Name)
+	}
+
+	return nil
+}
+
+func CommandMapBack(cfg *Config) error {
+	if cfg.Previous == "" {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+
+	locationResp, err := client.ListLocationAreas(cfg.Previous)
+	if err != nil {
+		return err
+	}
+
+	if locationResp.Next != nil {
+		cfg.Next = *locationResp.Next
+	} else {
+		cfg.Next = ""
+	}
+
+	if locationResp.Previous != nil {
+		cfg.Previous = *locationResp.Previous
+	} else {
+		cfg.Previous = ""
+	}
+
+	for _, location := range locationResp.Results {
+		fmt.Println(location.Name)
+	}
+
+	return nil
+}
+
+func CommandExplore(cfg *Config) error {
+	if len(cfg.Args) == 0 {
+		return fmt.Errorf("please provide a location area name or id to explore")
+	}
+
+	locationName := cfg.Args[0]
+	fmt.Printf("Exploring %s...\n", locationName)
+
+	locationArea, err := client.GetLocationArea(locationName)
+	if err != nil {
+		return fmt.Errorf("error exploring location area: %w", err)
+	}
+
+	fmt.Printf("Pokemon in %s:\n", locationArea.Name)
+	for _, pokemon := range locationArea.PokemonEncounters {
+		fmt.Println(pokemon.Pokemon.Name)
+	}
+	return nil
+}
+
+func CommandCatch(cfg *Config) error {
+	if len(cfg.Args) == 0 {
+		return fmt.Errorf("please provide a pokemon name")
+	}
+
+	pokemonName := cfg.Args[0]
+
+	if _, ok := cfg.CaughtPokemon[pokemonName]; ok {
+		return fmt.Errorf("you've already caught %s", pokemonName)
+	}
+
+	fmt.Printf("Throwing a Pokeball at %s...\n", pokemonName)
+
+	pokemon, err := client.GetPokemon(pokemonName)
+	if err != nil {
+		return err
+	}
+
+	catchRate := 0.5 - float64(pokemon.BaseExperience)/1000.0
+	if catchRate < 0.1 {
+		catchRate = 0.1
+	}
+
+	r := rand.Float64()
+	if r <= catchRate {
+		cfg.CaughtPokemon[pokemonName] = pokemon
+		fmt.Printf("%s was caught!\n", pokemonName)
+		return nil
+	}
+	fmt.Printf("%s escaped!\n", pokemonName)
+	return nil
+}
--- main.go
+++ main.go
@@ -3,63 +3,17 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math/rand/v2"
+	"github.com/AlexTLDR/GoPokedex/internal/pokeapi"
 	"os"
 	"strings"
 
-	"github.com/AlexTLDR/GoPokedex/internal/pokeapi"
+	"github.com/AlexTLDR/GoPokedex/internal/commands"
 )
 
-type commandFunc func(cfg *config) error
-
-type commandWithDescription struct {
-	exec        commandFunc
-	description string
-}
-
-var commands map[string]commandWithDescription
-var client pokeapi.Client
-
-func init() {
-	client = pokeapi.New()
-
-	commands = map[string]commandWithDescription{
-		"map": {
-			exec:        commandMap,
-			description: "Display the names of 20 location areas in the Pokemon world",
-		},
-		"mapb": {
-			exec:        commandMapBack,
-			description: "Go back to the previous list of location areas",
-		},
-		"help": {
-			exec:        commandHelp,
-			description: "Displays a help message",
-		},
-		"exit": {
-			exec:        commandExit,
-			description: "Exit the Pokedex",
-		},
-		"explore": {
-			exec:        commandExplore,
-			description: "Explore the Pokemon world",
-		},
-		"catch": {
-			exec:        commandCatch,
-			description: "Attempt to catch a Pokemon",
-		},
-	}
-}
-
-type config struct {
-	Next          string
-	Previous      string
-	Args          []string
-	CaughtPokemon map[string]pokeapi.Pokemon
-}
-
 func main() {
-	cfg := &config{
+	commands.Initialize()
+
+	cfg := &commands.Config{
 		CaughtPokemon: make(map[string]pokeapi.Pokemon),
 	}
 	scanner := bufio.NewScanner(os.Stdin)
@@ -85,20 +39,11 @@ func main() {
 		cmdName := parts[0]
 		args := parts[1:]
 
-		if cmd, exists := commands[cmdName]; exists {
-			err := executeCommand(cmd, cfg, args)
-			if err != nil {
-				fmt.Println("Error:", err)
-			}
-		} else {
-			fmt.Println("Unknown command. Type 'help' for a list of commands.")
+		err := commands.ExecuteCommand(cmdName, cfg, args)
+		if err != nil {
+			fmt.Println("Error:", err)
 		}
 	}
-}
-
-func executeCommand(cmd commandWithDescription, cfg *config, args []string) error {
-	cfg.Args = args
-	return cmd.exec(cfg)
 }
 
 func cleanInput(text string) []string {
@@ -112,126 +57,4 @@ func cleanInput(text string) []string {
 		cleaned = append(cleaned, strings.ToLower(part))
 	}
 	return cleaned
-}
-
-func commandExit(cfg *config) error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp(cfg *config) error {
-	fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
-	for name, cmd := range commands {
-		fmt.Printf("%s: %s\n", name, cmd.description)
-	}
-	return nil
-}
-
-func commandMap(cfg *config) error {
-	locationResp, err := client.ListLocationAreas(cfg.Next)
-	if err != nil {
-		return err
-	}
-
-	if locationResp.Next != nil {
-		cfg.Next = *locationResp.Next
-	} else {
-		cfg.Next = ""
-	}
-
-	if locationResp.Previous != nil {
-		cfg.Previous = *locationResp.Previous
-	} else {
-		cfg.Previous = ""
-	}
-
-	for _, location := range locationResp.Results {
-		fmt.Println(location.Name)
-	}
-
-	return nil
-}
-
-func commandMapBack(cfg *config) error {
-	if cfg.Previous == "" {
-		fmt.Println("you're on the first page")
-		return nil
-	}
-
-	locationResp, err := client.ListLocationAreas(cfg.Previous)
-	if err != nil {
-		return err
-	}
-
-	if locationResp.Next != nil {
-		cfg.Next = *locationResp.Next
-	} else {
-		cfg.Next = ""
-	}
-
-	if locationResp.Previous != nil {
-		cfg.Previous = *locationResp.Previous
-	} else {
-		cfg.Previous = ""
-	}
-
-	for _, location := range locationResp.Results {
-		fmt.Println(location.Name)
-	}
-
-	return nil
-}
-
-func commandExplore(cfg *config) error {
-	if len(cfg.Args) == 0 {
-		return fmt.Errorf("pleasGET https://pokeapi.co/api/v2/pokemon/{id or name}/e provide a location area name or id to explore")
-	}
-
-	locationName := cfg.Args[0]
-	fmt.Printf("Exploring %s...\n", locationName)
-
-	locationArea, err := client.GetLocationArea(locationName)
-	if err != nil {
-		return fmt.Errorf("error exploring location area: %w", err)
-	}
-
-	fmt.Printf("Pokemon in %s:\n", locationArea.Name)
-	for _, pokemon := range locationArea.PokemonEncounters {
-		fmt.Println(pokemon.Pokemon.Name)
-	}
-	return nil
-}
-
-func commandCatch(cfg *config) error {
-	if len(cfg.Args) == 0 {
-		return fmt.Errorf("please provide a pokemon name")
-	}
-
-	pokemonName := cfg.Args[0]
-
-	if _, ok := cfg.CaughtPokemon[pokemonName]; ok {
-		return fmt.Errorf("you've already caught %s", pokemonName)
-	}
-
-	fmt.Printf("Throwing a Pokeball at %s...\n", pokemonName)
-
-	pokemon, err := client.GetPokemon(pokemonName)
-	if err != nil {
-		return err
-	}
-
-	catchRate := 0.5 - float64(pokemon.BaseExperience)/1000.0
-	if catchRate < 0.1 {
-		catchRate = 0.1
-	}
-
-	r := rand.Float64()
-	if r <= catchRate {
-		cfg.CaughtPokemon[pokemonName] = pokemon
-		fmt.Printf("%s was caught!\n", pokemonName)
-		return nil
-	}
-	fmt.Printf("%s escaped!\n", pokemonName)
-	return nil
 }
